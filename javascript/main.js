4352dfe53cd4a0f4829fdb704d5cc40ea8452d74
Morgan-free <mwairym06@gmail.com>
2025-06-02 17:35:42 +0300

    first commit

---
+++ javascript/main.js
@@ -0,0 +1,8 @@
+document.getElementById("Run").onclick = function () {
+    let sal = document.getElementById("salary").Value;
+
+    let rate = 0;
+    let amount = salary - rate;
+
+    document.getElementById("out").innerText = "kshs" + amount;
+}
