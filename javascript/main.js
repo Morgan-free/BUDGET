b7e9723ba75dd110c7d48c48bbc2be07ccb4e722
Morgan-free <mwairym06@gmail.com>
2025-06-03 20:05:41 +0300

    #  modified:   CSS/style.css 	# modified:   index.html 	# modified:   javascript/main.js 	# new file:   jsconfig.json 	# modified:   notes

--- javascript/main.js
+++ javascript/main.js
@@ -1,8 +1,27 @@
-document.getElementById("Run").onclick = function () {
-    let sal = document.getElementById("salary").Value;
+document.getElementById("run").onclick = function () {
+    let salary = document.getElementById("salary").value;
 
-    let rate = 0;
-    let amount = salary - rate;
+    let expense = document.getElementById("renti").value;
+      
+    salary = salary - expense;
+    document.getElementById("rentbill").innerText = "rentbill" + "" + expense;
+    document.getElementById("rent").innerText = "salary remaining after renti" + salary;
+    // console.log("salary remaining afte rent", salary);
 
-    document.getElementById("out").innerText = "kshs" + amount;
-}
+    document.getElementById("electricity").value;
+    salary = salary - expense;
+    document.getElementById("electricitybill").innerText = "electricitybill" + "" + expense;
+    document.getElementById("electricitii").innerText = "salary remaining after electricity" + "" + salary;
+    
+     document.getElementById("fee").value;
+     salary = salary - expense;
+     document.getElementById("feesamount").innerText = "electricitybill" + "" + expense;
+     document.getElementById("fees").innerText = "salary remaining after electricity" + "" + salary;
+    
+     document.getElementById("save").value;
+     salary = salary - expense;
+     document.getElementById("savingamount").innerText = "electricitybill" + "" + expense;
+     document.getElementById("savings").innerText = "salary remaining after electricity" + "" + salary;
+    
+    
+};
